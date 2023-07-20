3aa4319968deb5c8083f1b0c9c91eef3f203bd22
DimaVarek <dimarvarrek@gmail.com>
2023-07-20 17:10:23 +0300

    Exam commit

--- APIManager.js
+++ APIManager.js
@@ -22,21 +22,23 @@ class APIManager {
             quote: "",
             pokemon: {
                 name: "",
-                picture: ""
+                picture: "",
+                pokemonGif: ""
             },
             aboutMe: ""
         }        
     }
-    refresh(){
-        const randomUsersPromise = this._randomUsersRequest()
+    async refresh(){
+        const randomUsersPromise = this._randomUsersRequest(7)
         const randomQuotePromise = this._randomQuoteRequest()
-        const randomPokemonPromise = this._randomPokemonRequest()
         const randomAboutMePromise = this._randomAboutMeRequest()
+        const randomPokemonPromise = await this._randomPokemonRequest()
+        const pokemonGifPromise = this._gifByPokemon()
         return Promise.all([randomUsersPromise, randomQuotePromise,
-        randomPokemonPromise, randomAboutMePromise])       
+            pokemonGifPromise, randomAboutMePromise])
     }
-    async _randomUsersRequest() {
-        let randomUsers = await $.get('https://randomuser.me/api/?results=7')
+    async _randomUsersRequest(usersNumber) {
+        let randomUsers = await $.get(`https://randomuser.me/api/?results=${usersNumber}`)
         let randomUsersArray = randomUsers.results.map(u => this.getUserData(u))
         this.data.mainUser = randomUsersArray[0]
         this.data.friends = randomUsersArray.slice(1)
@@ -53,6 +55,12 @@ class APIManager {
     async _randomQuoteRequest() {
         let randomQuote = await $.get('https://api.kanye.rest/')
         this.data.quote = randomQuote.quote
+    }
+    async _gifByPokemon() {
+        let apiKey = 'bZvle0mAld1pODg1HUfinKCCqI5enSwy'
+        let request = `http://api.giphy.com/v1/gifs/search?q=pokemon+${this.data.pokemon.name}&api_key=${apiKey}&limit=1`
+        let pokemonGif = await $.get(request)
+        this.data.pokemon.pokemonGif = pokemonGif.data[0].images.original.url
     }
     async _randomPokemonRequest() {
         let randomPokemonNumber = Math.floor(Math.random() * 1010) + 1
--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const renderer = new Renderer()
 function refresh () {
     apiManager.refresh()
         .then(() => renderer.refresh(apiManager.data))
+        .catch(() => alert('Oops! Something went wrong! Update the page and write to the mail: idontcare@dontcare.co.il'))
 }
 
 $('button'). click(function () {
